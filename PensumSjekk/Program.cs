6c57eb505483653b1a5aec1f026003ddf3f3e54a
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-16 14:58:51 +0100

    æ

---
+++ PensumSjekk/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PensumSjekk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
