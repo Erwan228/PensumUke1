ed548688ca09e01fced2543282cadbfc86e02e6d
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-17 13:54:58 +0100

    Pappas challenge

--- PensumSjekk/Program.cs
+++ PensumSjekk/Program.cs
@@ -1,16 +1,34 @@
 ﻿namespace PensumSjekk
 {
+   
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hvem vil ha en ny hobby?");
             string name = Console.ReadLine();
-            string[] hobbies = { "Fursuiter", "Illustratør", "Sanger", "Dancer", "Musiker" };
-            Random rnd = new Random();
+            MakeHobby( name);
+            Main(null);
+            }
+            
+        
+        static void MakeHobby (string name)
+        {
+        string[] hobbies = { "Fursuiter", "Illustratør", "Sanger", "Dancer", "Musiker" };
+        Random rnd = new Random();
             int rng = rnd.Next(hobbies.Length);
             Console.WriteLine($"{name}s nye hobby er {hobbies[rng]}");
-            Main(null);
+            Console.WriteLine("Er du fornøyd? (true/false)");
+            string answer = Console.ReadLine();
+            bool pleasure = Convert.ToBoolean(answer);
+            if (pleasure) {
+                Console.WriteLine($"Wonderful! {name} er nå {hobbies[rng]}");
+                return; }
+             else
+            {
+                MakeHobby(name);
+            }
         }
     }
 }
